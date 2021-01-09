110d6ad908797ea626a2d17ec423b2d69a52f25d
Fernando Garbo <nando.garbo@gmail.com>
2021-01-09 17:41:46 -0300

    POST /orders

--- backend/src/main/java/com/garbo/dsdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/garbo/dsdeliver/repositories/OrderRepository.java
@@ -10,6 +10,6 @@ import com.garbo.dsdeliver.entities.Order;
 public interface  OrderRepository extends JpaRepository<Order, Long>{
 
 	@Query("SELECT DISTINCT obj FROM Order obj JOIN FETCH obj.products "
-			+ "WhERE obj.status = 0 ORDER BY obj.moment ASC")
+			+ "WHERE obj.status = 0 ORDER BY obj.moment ASC")
 	List<Order> findOrdersWithProducts();
 }
--- backend/src/main/java/com/garbo/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/garbo/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.garbo.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.garbo.dsdeliver.dto.OrderDTO;
+import com.garbo.dsdeliver.dto.ProductDTO;
 import com.garbo.dsdeliver.entities.Order;
+import com.garbo.dsdeliver.entities.OrderStatus;
+import com.garbo.dsdeliver.entities.Product;
 import com.garbo.dsdeliver.repositories.OrderRepository;
+import com.garbo.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,10 +22,25 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true) //Para evitar lock no banco já que é só leitura
 	public List<OrderDTO> findAll(){
 		List<Order> list = repository.findOrdersWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList()); //Convertendo a lista de produtos para DTO. Primeiro para stream e depois volta para lista
 		
 	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), 
+				Instant.now(), OrderStatus.PENDING);
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId()); //Cria uma entidade gerenciada pelo JPA para salvar as associações order/product
+			order.getProducts().add(product);
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);		
+	}	
 }
