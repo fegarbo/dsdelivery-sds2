4835644a9c87b57135321676cc245765bbdb9341
Fernando Garbo <nando.garbo@gmail.com>
2021-01-10 18:25:06 -0300

    Search Order location

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,27 @@
-import StepsHeader from './StepsHeader';
+import { useEffect, useState } from 'react';
 import './styles.css';
+import ProductsList from './ProductsList';
+import StepsHeader from './StepsHeader';
+import { Product } from './types';
+import { OrderLocationData } from './types';
+import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
 
 function Orders(){
-    return(
+    const [products, setProducts] = useState<Product[]>([]);
+    const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
+
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
+    return( //Quando o componente tem Prop, deve ser implementado como exemplos abaixo (ProductList, Orderlocation)
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products} />
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
         </div>
     );
 }
