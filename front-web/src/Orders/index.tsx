2d956d531abcb029c9d38346f4033235dbf70bfc
Fernando Garbo <nando.garbo@gmail.com>
2021-01-10 19:49:03 -0300

    Finish front web

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,28 +1,78 @@
 import { useEffect, useState } from 'react';
-import './styles.css';
+import { toast } from 'react-toastify';
 import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
 import { Product } from './types';
 import { OrderLocationData } from './types';
-import { fetchProducts } from '../api';
+import { fetchProducts, saveOrder } from '../api';
 import OrderLocation from './OrderLocation';
+import OrderSummary from './OrderSummary';
+import Footer from '../Footer';
+import { checkIsSelected } from './helpers';
+import './styles.css';
 
-function Orders(){
+function Orders() {
     const [products, setProducts] = useState<Product[]>([]);
+    const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
     const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
+    const totalPrice = selectedProducts.reduce((sum, item) => {
+        return sum + item.price;
+    }, 0)
 
     useEffect(() => {
         fetchProducts()
             .then(response => setProducts(response.data))
-            .catch(error => console.log(error))
+            .catch(error => {
+                toast.warning('Erro ao listar produtos');
+            })
     }, []);
 
-    return( //Quando o componente tem Prop, deve ser implementado como exemplos abaixo (ProductList, Orderlocation)
-        <div className="orders-container">
-            <StepsHeader />
-            <ProductsList products={products} />
-            <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
-        </div>
+    const handleSelectProduct = (product: Product) => {
+        const isAlreadySelected = checkIsSelected(selectedProducts, product);
+
+        if (isAlreadySelected) {
+            const selected = selectedProducts.filter(item => item.id !== product.id);
+            setSelectedProducts(selected);
+        } else {
+            setSelectedProducts(previous => [...previous, product]);
+        }
+    }
+
+    const handleSubmit = () => {
+        const productsIds = selectedProducts.map(({ id }) => ({ id }));
+        const payload = {
+            ...orderLocation!,
+            products: productsIds
+        }
+
+        saveOrder(payload)
+            .then((response) => {
+                toast.error(`Pedido enviado com sucesso! Nº ${response.data.id}`); //usando toast error pelo padrão de cores da app
+                setSelectedProducts([]);
+            })
+            .catch(() => {
+                toast.warning('Erro ao enviar pedido');
+            })
+    }
+
+    return ( //Quando o componente tem Prop, deve ser implementado como exemplos abaixo (ProductList, Orderlocation)
+        <>
+            <div className="orders-container">
+                <StepsHeader />
+                <ProductsList
+                    products={products}
+                    onSelectProduct={handleSelectProduct}
+                    selectedProducts={selectedProducts}
+                />
+                <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
+                <OrderSummary
+                    amount={selectedProducts.length}
+                    totalPrice={totalPrice}
+                    onSubmit={handleSubmit}
+                />
+            </div>
+            <Footer />
+        </>
     );
 }
 
